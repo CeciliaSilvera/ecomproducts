49af393ebf4893afbceae9b5a5193f616f63dfaa
Cecilia Silvera <cecilia.silvera@1337.tech>
2022-06-18 17:00:16 +0200

    base product listing and style

--- ecomproducts/src/components/Products.vue
+++ ecomproducts/src/components/Products.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    <h1>Products</h1>
-    <div
-      v-for="(prod, idx) in allProducts"
-      :key="idx"
-    >
-      {{ prod.title }}
+    <div class="heading">
+      <h1>Products</h1>
+    </div>
+    <div class="product-list">
+      <div
+        v-for="(prod, idx) in allProducts"
+        :key="idx"
+        class="product"
+      >
+        <div
+          class="thumbnail"
+          :style="{ backgroundImage: `url(${prod.image})` }"
+        />
+        <div class="summary">
+          <h4>{{ prod.title }}</h4>
+          <p>price: {{ prod.price }}</p>
+          <p>category: {{ prod.category }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,3 +37,34 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+  .product {
+    box-shadow: 0 0 4px #ccc;
+    border: 1px solid #ccc;
+    padding: 1.5rem;
+    overflow: hidden;
+    cursor: pointer;
+    display: grid;
+    grid-template-rows: 1fr 2fr;
+    grid-row-gap: 2rem;
+  }
+  
+  .thumbnail {
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+  }
+  
+  .summary {
+    text-align: left;
+    display: flex;
+    flex-flow: column;
+  }
+  
+  .product-list {
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(auto-fill, 15rem);
+  }
+</style>
