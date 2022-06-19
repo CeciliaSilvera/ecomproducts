a80ba1c598f5bc9d508a3a104a4810924f966b1e
Cecilia Silvera <cecilia.silvera@1337.tech>
2022-06-19 02:44:41 +0200

    add dirty pagination

--- ecomproducts/src/components/Products.vue
+++ ecomproducts/src/components/Products.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
+    <div v-if="chunks.length > 1">
+      <button
+        type="button"
+        aria-label="show prev"
+        class="pagination-btn"
+        @click="goPrev"
+      >
+        Prev
+      </button>
+      <button
+        type="button"
+        aria-label="show next"
+        class="pagination-btn"
+        @click="goNext"
+      >
+        Next
+      </button>
+    </div>
     <div class="product-list">
       <product-preview
-        v-for="(product, idx) in activeProducts"
+        v-for="(product, idx) in displayProducts"
         :key="idx"
         ref="product-preview"
         :product="product"
@@ -23,12 +41,45 @@ import ProductPreview from "@/components/ProductPreview";
 export default {
   name: "Products",
   components: {
-    ProductPreview 
+    ProductPreview
+  },
+  data() {
+    return {
+      displayProducts: [],
+      chunkIndex: 0,
+      chunks: []
+    };
   },
   computed:{
     ...mapGetters({
       activeProducts: ACTIVE_PRODUCTS
     })
+  },
+  watch: {
+    activeProducts() {
+      this.chunks = this.activeProducts.reduce((prev, curr, index) => {
+        const chunkIndex = Math.floor(index/8);
+        if (!prev[chunkIndex]) {
+          prev[chunkIndex] = []; // start a new chunk
+        }
+        prev[chunkIndex].push(curr);
+        return prev;
+      }, []);
+      this.displayProducts = this.chunks[this.chunkIndex];
+    },
+    chunkIndex() {
+      this.displayProducts = this.chunks[this.chunkIndex];
+    }
+  },
+  methods:{
+    goNext() {
+      if (this.chunks.length === this.chunkIndex +1) return false;
+      this.chunkIndex += 1;
+    },
+    goPrev() {
+      if (this.chunkIndex === 0) return false;
+      this.chunkIndex -= 1;
+    }
   }
 };
 </script>
@@ -38,5 +89,11 @@ export default {
     display: grid;
     gap: 1rem;
     grid-template-columns: repeat(auto-fill, 15rem);
+  }
+  .pagination-btn {
+    padding: 1rem;
+    border: 1px solid #ccc;
+    color: #2c3e50;
+    background-color: #f0fff0;
   }
 </style>
